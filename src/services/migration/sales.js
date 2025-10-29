21904c1d465225b2702fd5944af259eabaa88a36
pepe <josepp2508@gmail.com>
2025-10-28 23:03:32 -0600

    [IMP] search for products in order lines

--- src/services/migration/sales.js
+++ src/services/migration/sales.js
@@ -12,40 +12,55 @@ export const migrateSales = async () => {
                 "name",
                 "partner_id",
                 "state",
+                "date_order",
+                "order_line"
             ],
             [],
             3
         );
 
-        //console.log(sales)
+        for (const sale of sales) {
+            console.log({sale});
+
+            const lineIds = sale.order_line;
+            const orderLines = await origin.readModel(
+                originDb,
+                "sale.order.line",
+                ["product_template_id", "product_uom_qty", "qty_delivered", "qty_invoiced", "price_unit", "tax_id", "price_subtotal"],
+                [[["id", "in", lineIds]]]
+            );
+
+            console.log({orderLines});
+        }
 
         let created = 0;
         let failed = 0;
 
-        for (const sale of sales) {
-            console.log(`Looking for partner: ${sale.partner_id[1]}`);
-            try {
-                const contact = await dest.readModel(
-                    destinationDb,
-                    "res.partner",
-                    ["id", "name"],
-                    [[["name", "ilike", sale.partner_id[1]]]],
-                    1
-                );
+        // for (const sale of sales) {
+        //     console.log(`Looking for partner: ${sale.partner_id[1]}`);
+        //     try {
+        //         const contact = await dest.readModel(
+        //             destinationDb,
+        //             "res.partner",
+        //             ["id", "name"],
+        //             [[["name", "ilike", sale.partner_id[1]]]],
+        //             1
+        //         );
 
-                const newSale = await dest.createModel(destinationDb, "sale.order", {
-                    name: sale.name,
-                    partner_id: contact[0].id,
-                    state: sale.state,
-                });
+        //         const newSale = await dest.createModel(destinationDb, "sale.order", {
+        //             name: sale.name,
+        //             partner_id: contact[0].id,
+        //             state: sale.state,
+        //             date_order: sale.date_order
+        //         });
 
-                created++;
-                console.log(`Sale created: ${newSale}`);
-            } catch (error) {
-                console.error(error.message);
-                failed++;
-            }
-        }
+        //         created++;
+        //         console.log(`Sale created: ${newSale}`);
+        //     } catch (error) {
+        //         console.error(error.message);
+        //         failed++;
+        //     }
+        // }
 
         console.log(`Sales migrated: ${created}, failed: ${failed}`);
     } catch (error) {
